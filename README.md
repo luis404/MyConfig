1e208d495cbde3c3a1db70e783edda8d74e1591f
luis404 <luisxu404@gmail.com>
2015-04-11 22:48:22 +0800

    change README.md to README.org, delete ssh

--- README.md
+++
@@ -1,2 +0,0 @@
-# MyConfig
-MacOS/Linux Config
--- auto_install.sh
+++ auto_install.sh
@@ -1,29 +1,8 @@
 #!/bin/sh
 
-# create the dir than I worked in
-LXDIR="~/lx/"
-mkdir $LXDIR
+#backup the original files if neccesary
+# do_backup
 
-cd $LXDIR
-
-# create emacs configuration 
-cp -rf .emacs ~/.emacs
-cp -rf emacs ${LXDIR}
-cp -rf emacs.d/* ~/.emacs.d
-
-# install oh-my-zsh
-curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
-
-# vimrc
-cp -rf vimrc ~/.vimrc
-
-# zshrc
-cp -rf zshrc ~/.zshrc
-
-# install homebrew
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-
-# clone projects from github
-mkdir github
-cd github
-git clone git@github.com:luis404/luis404.github.io.git blog
+source emacs/emacs.sh
+source vim/vim.sh
+source git/git.sh
