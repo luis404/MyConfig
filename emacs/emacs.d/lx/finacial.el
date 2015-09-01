b5b8dd3735c06f6b654614e277fc32a33e2cb3ea
luis404 <luisxu404@gmail.com>
2015-09-01 10:08:08 +0800

    update

--- emacs/emacs.d/lx/finacial.el
+++ emacs/emacs.d/lx/finacial.el
@@ -2,25 +2,14 @@
 (setq lxm-file "~/Dropbox/lx/finacial")
 (setq lxm-hash (make-hash-table))
 
+;; read file, return a split list for every line
 (defun lxm-read-file (filename)
   (with-temp-buffer
     (insert-file-contents filename)
     (split-string (buffer-string) "\n" t "[,]")))
 
-
+;; turn list elements to hashmap elements
 (defun lxm-build-hash-table (strlists)
-  (let ((key nil)
-	(value nil))
-    (dolist (elt strlists)
-      (if (string-match ":" elt) (setq value (cons elt value))
-	((if key ((puthash lxm-hash key value)
-		  (setq value nil)))
-	 (setq key elt))
-	(print value)))))
-
-
-(defun lxm-test-if ()
-  (if '(1)
-      ((print "1")
-    (print "2"))
-    (print "3")))
+  (let ((value nil) (key nil))
+    (dolist (item strlists)
+      (if ()))))
